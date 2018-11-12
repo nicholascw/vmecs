fbcbb1078be7e21c3adb9c236326874e4b265178
rod-lin <zl38@illinois.edu>
2018-11-13 00:07:31 +0800

    fix fd_read/write

--- pub/fd.h
+++ pub/fd.h
@@ -12,7 +12,7 @@ INLINE ssize_t
 fd_read(fd_t fd, byte_t *buf, size_t size)
 {
     ssize_t ret;
-    while ((ret = read(fd, buf, size)) == -1 && (errno == EINTR || errno == EAGAIN));
+    while ((ret = read(fd, buf, size)) == -1 && (errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK));
     return ret;
 }
 
@@ -20,7 +20,7 @@ INLINE ssize_t
 fd_write(fd_t fd, const byte_t *buf, size_t size)
 {
     ssize_t ret;
-    while ((ret = write(fd, buf, size)) == -1 && (errno == EINTR || errno == EAGAIN));
+    while ((ret = write(fd, buf, size)) == -1 && (errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK));
     return ret;
 }
 
